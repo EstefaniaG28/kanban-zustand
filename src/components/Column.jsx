bd9cb58133ecea1475d42e812018e3b5cee88fce
Estefania <estefaniagutierrezcardozo@gmail.com>
2023-06-11 17:58:37 +0100

    state

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,7 +1,17 @@
+import { useMemo } from "react";
+import { useStore } from "../store";
 import "./Column.css";
 import Task from "./Task";
 
 export default function Column({ state }) {
+  const tasks = useStore(
+    (store) => store.task //.filter(task=>task.state===state)
+  );
+  const filtered = useMemo(
+    () => tasks.filter((task) => task.state === state),
+    [tasks, state]
+  );
+
   return (
     <div className="column">
       <p>{state}</p>
